80fa762a34eb12fb8cb4ef30918ee2e965c97378
Piotr Kedziora <piotrkedziorapl@gmail.com>
2019-02-24 23:28:03 +0100

    Add script to slider

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//menu
+//MENU
 $('#hamburger').click(function(){
     $('.navbar__menu--main').slideToggle();
 });
@@ -10,3 +10,26 @@ $('.navbar__link').click(function(){
 $(window).resize(function(){
     if($(window).width() > 992) $('.navbar__menu--main').removeAttr('style');
 });
+
+//SLIDER
+//progress
+var slides = $('.slider__item');
+var index = 0;
+var progress = $('.slider__progress');
+function changeSlide() {
+    progress.css({
+        transition: 'none',
+        width: 0+'%'
+    });
+    slides.eq(index).addClass('slider__item--hide');
+    (index == slides.length - 1) ? index = 0 : index += 1;
+    slides.eq(index).removeClass('slider__item--hide');
+    progressBar();
+}
+function progressBar() {
+    progress.animate({width: 100+'%'},5000,'linear',function(){
+        changeSlide();
+    });
+}
+progressBar();
+
