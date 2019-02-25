ce92f59f915f58d55efa1bfbe952b8ecd2dc2ef3
Piotr Kedziora <piotrkedziorapl@gmail.com>
2019-02-25 10:03:32 +0100

    Add intorducing section

--- js/script.js
+++ js/script.js
@@ -33,3 +33,32 @@ function progressBar() {
 }
 progressBar();
 
+//SLICK INTORDUCE
+
+$('.member-slider__list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    nextArrow: $('.member-slider__arrow-next'),
+    prevArrow: $('.member-slider__arrow-prev'),
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+  });
